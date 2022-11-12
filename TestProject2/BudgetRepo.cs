7b50f81522a4363b6311d8cc5c12935d5e17e2f6
StevenHuang93 <32432894+StevenHuang93@users.noreply.github.com>
2022-11-12 16:16:59 +0800

    加一個class

---
+++ TestProject2/BudgetRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject2
+{
+    public class BudgetRepo : IBudgetRepo
+    {
+        public List<Budget> GetAll()
+        {
+            return new List<Budget>();
+        }
+    }
+}

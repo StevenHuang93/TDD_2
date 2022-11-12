1895c852cbee3e84bcff2dcbef25e2f1d49501ee
StevenHuang93 <32432894+StevenHuang93@users.noreply.github.com>
2022-11-12 17:18:59 +0800

    Bug fixed.

--- TestProject2/BudgetService.cs
+++ TestProject2/BudgetService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,7 +21,7 @@ namespace TestProject2
         {
             var result = _budget.GetAll();
 
-            var queryStart = start.ToString("yyyyMM");
+
 
             var queryEnd = end.ToString("yyyyyMM");
 
@@ -28,25 +29,81 @@ namespace TestProject2
 
             if (start.Year == end.Year && start.Month == end.Month)
             {
-                var diff = end.Date - start.Date;
+                reuslt = GetDayBudget(start, end);
+            }
+            else
+            {
+                int month = GetMonth(start, end);
 
-                var diffStart = diff.Days + 1; // 同年月跨日
-
-                var monthsday = DateTime.DaysInMonth(start.Year, start.Month); // 當月有幾天
-
-                var budgetResult = result.FirstOrDefault(a => a.YearMonth == queryStart);
-
-                if (budgetResult != null)
+                for (int i = 0; i <= month; i++)
                 {
-                    reuslt = (diffStart / monthsday) * budgetResult.Amount;
+                    if (i == 0)
+                    {
+                        reuslt += GetDayBudget(start, new DateTime(start.Year, start.Month, 1).AddMonths(1).AddDays(-1));
+                    }
+                    else if (i == month)
+                    {
+                        reuslt += GetDayBudget(new DateTime(end.Year, end.Month, 1), end);
+                    }
+                    else
+                    {
+                        var temp = start.AddMonths(i);
+                        reuslt += GetDayBudget(new DateTime(temp.Year, temp.Month, 1), new DateTime(temp.Year, temp.Month, 1).AddMonths(1).AddDays(-1));
+                    }
                 }
-
-
             }
 
             return reuslt;
         }
 
-        
+        public int GetMonth(DateTime start, DateTime end)
+        {
+            var mCount = 0;
+            var sYM = start.Year * 100 + start.Month;  //202202
+            var eYM = end.Year * 100 + end.Month;   //202302
+            while (sYM < eYM)
+            {
+                var YM = start.AddMonths(++mCount);
+                sYM = YM.Year * 100 + YM.Month;
+
+            }
+
+            return mCount;
+
+        }
+
+        /// <summary>
+        /// 同年月跨日
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        public decimal GetDayBudget(DateTime start, DateTime end)
+        {
+
+            decimal result = 0;
+
+            var queryStart = start.ToString("yyyyMM");
+
+            var diff = end.Date - start.Date;
+
+            decimal diffStart = diff.Days + 1; // 同年月跨日
+
+            var monthsday = DateTime.DaysInMonth(start.Year, start.Month); // 當月有幾天
+
+            var budgetResult = _budget.GetAll().FirstOrDefault(a => a.YearMonth == queryStart);
+
+            if (budgetResult != null)
+            {
+                result = (diffStart * budgetResult.Amount / monthsday);
+            }
+            else
+            {
+                result = 0;
+            }
+
+            return result;
+
+        }
     }
 }
--- TestProject2/UnitTest1.cs
+++ TestProject2/UnitTest1.cs
@@ -34,5 +34,34 @@ namespace TestProject2
 
             Assert.AreEqual(3100m, result);
         }
+
+        [Test]
+        public void ¸ó¦~()
+        {
+            var a = new List<Budget>()
+            {
+                new Budget()
+                {
+                     YearMonth="202212", Amount=3100
+                },
+                new Budget()
+                {
+                     YearMonth="202301", Amount=310
+                },
+                               new Budget()
+                {
+                     YearMonth="202302", Amount=28
+                },
+            };
+
+            _budgetRepo.GetAll().Returns(a);
+
+            var service = new BudgetService(_budgetRepo);
+
+            var result = service.Query(new System.DateTime(2022, 12, 23), new System.DateTime(2023, 2, 12));
+
+            Assert.AreEqual(1222m, result);
+        }
+
     }
 }

34d902781072fe38addb4b48563d40f9a87023a3
StevenHuang93 <32432894+StevenHuang93@users.noreply.github.com>
2022-11-12 17:21:32 +0800

    增加單元測試

--- TestProject2/UnitTest1.cs
+++ TestProject2/UnitTest1.cs
@@ -63,5 +63,60 @@ namespace TestProject2
             Assert.AreEqual(1222m, result);
         }
 
+        [Test]
+        public void 查一天()
+        {
+            var a = new List<Budget>()
+            {
+                new Budget()
+                {
+                     YearMonth="202212", Amount=3100
+                },
+                new Budget()
+                {
+                     YearMonth="202301", Amount=310
+                },
+                               new Budget()
+                {
+                     YearMonth="202302", Amount=28
+                },
+            };
+
+            _budgetRepo.GetAll().Returns(a);
+
+            var service = new BudgetService(_budgetRepo);
+
+            var result = service.Query(new System.DateTime(2022, 12, 15), new System.DateTime(2022, 12, 15));
+
+            Assert.AreEqual(100m, result);
+        }
+
+        [Test]
+        public void 非法的()
+        {
+            var a = new List<Budget>()
+            {
+                new Budget()
+                {
+                     YearMonth="202212", Amount=3100
+                },
+                new Budget()
+                {
+                     YearMonth="202301", Amount=310
+                },
+                               new Budget()
+                {
+                     YearMonth="202302", Amount=28
+                },
+            };
+
+            _budgetRepo.GetAll().Returns(a);
+
+            var service = new BudgetService(_budgetRepo);
+
+            var result = service.Query(new System.DateTime(2022, 12, 16), new System.DateTime(2022, 12, 15));
+
+            Assert.AreEqual(0m, result);
+        }
     }
 }
